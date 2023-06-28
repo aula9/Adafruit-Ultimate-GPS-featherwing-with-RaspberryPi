b6428fd8de9c222218e960e0724f9dd630bd29a5
Aula Jazmati <37100066+aula9@users.noreply.github.com>
2023-06-28 09:16:49 -0700

    Update GPS1.py

--- GPS1.py
+++ GPS1.py
@@ -2,6 +2,7 @@ import serial               #import serial pacakge
 from time import sleep
 ser = serial.Serial ("/dev/ttyS0", baudrate=9600, timeout=0.5)
 gpgga_info = "$GPGGA,"
+f = open("gps_data.txt", "w")
 while True:
         received_data = str(ser.readline())
         print(received_data)
@@ -13,7 +14,12 @@ while True:
             nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
             nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
             nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
-    
+            
+#             nmea_latitude = float(nmea_latitude )
+#             nmea_longitude  = float(nmea_longitude  )
             print("NMEA Time: ", nmea_time,'\n')
             print ("NMEA Latitude:", nmea_latitude,"NMEA Longitude:", nmea_longitude,'\n')
+            f.write(nmea_latitude + ","+ nmea_longitude)
+            
         sleep(1)
+f.close()
