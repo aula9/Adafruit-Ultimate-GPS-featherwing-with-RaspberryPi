b385c7108f1a2463a2881c4656bac1cd544e2173
Aula Jazmati <37100066+aula9@users.noreply.github.com>
2023-06-28 09:18:52 -0700

    Update GPS2.py

--- GPS2.py
+++ GPS2.py
@@ -1,26 +1,40 @@
-import serial               #import serial pacakge
+import serial              
 from time import sleep
-ser = serial.Serial ("/dev/ttyS0", baudrate=9600, timeout=0.5)
-gpgga_info = "$GPGGA,"
-output_file = open("gps_data.txt", "w")
-while True:
-        received_data = str(ser.readline())
-        print(received_data)
-        GPGGA_data_available = received_data.find(gpgga_info)
-        if (GPGGA_data_available>0):
-            GPGGA_buffer = received_data.split("$GPGGA,",1)[1]
-            NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
-            #print(NMEA_buff )
-            nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
-            nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
-            nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
+import sys
+import webbrowser
 
-        # Write latitude and longitude to output file
-            output_file.write(nmea_latitude + "," + nmea_longitude + "\n")
+ser = serial.Serial ('/dev/ttyS0')
+gpgga_info = '$GPGGA,'
+GPGGA_buffer = 0
+NMEA_buff = 0
+GPGGA_data_available = ''
 
-        # Wait a short period of time before reading again
-        
-            print("NMEA Time: ", nmea_time,'\n')
-            print ("NMEA Latitude:", nmea_latitude,"NMEA Longitude:", nmea_longitude,'\n')
-        sleep(0.1)
+def convert_to_degrees(raw_value):
+    decimal_value = raw_value/100.00
+    degrees = int(decimal_value)
+    mm_mmmm = (decimal_value - int(decimal_value))/0.6
+    position = degrees + mm_mmmm
+    position = '%.4f' %(position)
+    return position
 
+received_data = (str)(ser.read(200)) #read NMEA string received
+GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string
+if (GPGGA_data_available>0):
+    GPGGA_buffer = received_data.split('$GPGGA,',1)[1]  #store data coming after “$GPGGA,” string
+    NMEA_buff = (GPGGA_buffer.split(','))
+    nmea_time = []
+    nmea_latitude = []
+    nmea_longitude = []
+    nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
+    nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
+    nmea_longitude = NMEA_buff[3]
+    print('NMEA Time: ', nmea_time,'\n')
+    lat = (float)(nmea_latitude)
+    lat = convert_to_degrees(lat)
+    longi = (float)(nmea_longitude)
+    longi = convert_to_degrees(longi)
+    print ('NMEA Latitude:', lat,'NMEA Longitude:', longi,'\n')
+    map_link = 'http://maps.google.com/?q=' + lat + ',' + longi
+    webbrowser.open(map_link)
+    
+sys.exit(0)
